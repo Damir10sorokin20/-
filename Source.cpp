cf31a8c3309264aa2240a643bd3d6b1c191d7939
Damir10sorokin20 <114170327+Damir10sorokin20@users.noreply.github.com>
2022-10-06 18:39:12 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,214 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+//Дано целое число в диапазоне 1–7.Вывести строку — название дня
+		//недели, соответствующее данному числу(1 — «понедельник», 2 — «втор -
+			//18 М.Э.Абрамян.Электронный задачник Programming Taskbook 4.5ник» и т.д.).
+	//int day;
+	//cin >> day;
+	//if (day == 1) {
+		//cout<<"понедельник";
+
+	//}
+	//int day;
+	//cin >> day
+	// if (day == 2) {
+			//cout << "вторник";
+	//}
+	//int day;
+	//cin >> day
+ //if (day == 3) {
+			//cout << "среда";
+		
+			//int day;
+			//cin >> day
+	//else if (day == 4) {
+					//cout << "четверг";
+
+					//int day;
+					//cin >> day
+	//else if (day == 5) {
+		//cout << "пятница";
+
+
+		//int day;
+		//cin >> day
+	//else if (day == 6) {
+		//cout << "суббота";
+
+
+		//int day;
+		//cin >> day
+	//else if (day == 7) {
+		//cout << "воскресенье";
+	//cout << "ошибка" <<
+		//else{
+
+
+		//Case2.Дано целое число K.Вывести строку - описание оценки, соответствующей числу K(1 — «плохо», 2 — «неудовлетворительно», 3 — «удовлетворительно», 4 — «хорошо», 5 — «отлично»).Если K не лежит в диапазоне 1–5,
+			//то вывести строку «ошибка».
+	//int a;
+	//switch (a) {
+	//case 1: cout << "понедельник" << endl; break;
+	//case 2: cout << "вторник" << endl; break;
+	//case 3: cout << "среда" << endl; break;
+	//case 4: cout << "четверг" << endl; break;
+	//case 5: cout << "пятница" << endl; break;
+	//case 6: cout << "суббота" << endl; break;
+	//case 7: cout << "воскресенье" << endl; break;
+	//}
+	 //case3
+		//int month;
+	//cin >> month;
+	//switch (month) {
+	//case 1:
+		//case 2:
+		//case 12:cout << "зима" << endl; break;
+		//case 3:
+		//case 4:
+		//case 5:cout << "весна" << endl; break;
+		//case 6:
+		//case 7:
+		//case 8:cout << "лето" << endl; break;
+		//case 9:
+		//case 10:
+		//case 11: cout << "Осень" << endl; break;
+
+//}
+	//Case4◦
+		//.Дан номер месяца — целое число в диапазоне 1–12(1 — январь, 2 —
+			//февраль и т.д.).Определить количество дней в этом месяце для невисокосного года.
+
+	//int month;
+	//cin >> month;
+	//switch (month) {
+
+	//case 1: 
+	//case 2:
+	//case 12:cout << "Зима"; break;
+	//case 3:
+	//case 4:
+	//case 5:cout << "Весна"; break;
+	//case 6:
+	//case 7:
+	//case 8:cout << "Лето"; break;
+	//case 9:
+	//case 10:
+	//case 11:cout << "Осень"; break;
+		//if (month == 1) {
+			//cout << 31;
+		//}
+		//else if (month == 2) {
+			//cout << 28;
+		//}
+		//else if (month == 3) {
+			//cout << 31;
+		//}
+		//else if (month == 4) {
+			//cout << 30;
+
+		//else if (month == 5) {
+			//cout << 31;
+		//}
+
+		//else if (month == 6) {
+			//cout << 30;
+
+		//}
+
+		//else if (month == 7) {
+			//cout << 31;
+
+		//}
+
+		//else if (month == 8) {
+			//cout << 31;
+
+		//}
+
+		//else if (month == 9) {
+			//cout << 30;
+
+		//}
+
+		//else if (month == 10) {
+			//cout << 31;
+
+		//}
+
+		//else if (month == 11) {
+			//cout << 30;
+
+		//}
+		//else if (month == 12) {
+			//cout << 31;
+
+		//}
+
+
+		//}
+
+//Case5.Арифметические действия над числами пронумерованы следующим
+//образом : 1 — сложение, 2 — вычитание, 3 — умножение, 4 — деление.Дан
+//номер действия N(целое число в диапазоне 1–4) и вещественные числа A
+//и B(В не равно 0).Выполнить над числами указанное действие и вывести
+//результат.
+//int n;
+//float a, b;
+//cin >> a>>b>>n;
+
+//switch (n) {
+//case 1: cout << "Сложение"; a + b; break;
+//case 2: cout << "вычитание"; a - b; break;
+//case 3: cout << "умножение"; a* b; break;
+//case 4: cout << "деление"; a / b; default;
+//}
+//default << cout << 0;
+
+//1
+//int a, b, c;
+//cin >> a >> b >> c;
+//bool r;
+//r = a==b && b==c;
+//cout << boolalpha << r;
+
+ //2
+//int n;
+//cin >> n;
+//bool r;
+//r = n % 2 ==0 && n > 9 && n < 100;
+//cout << boolalpha << r;
+
+//3
+//int a, b, c;
+//cin >> a >> b >> c;
+//bool r;
+//r = a == b || b == c || c == b;
+//cout << boolalpha << r;
+
+//if1
+int a, b;
+cin >> a >> b;
+if(a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
